1b0d87cfc7bb60c08d3995302ee14ccf6e1ae5e1
Kyle Lomen <kylelomen@gmail.com>
2018-01-22 22:21:39 -0800

    Daemon mode working correctly now

--- reactor/reactor.lua
+++ reactor/reactor.lua
@@ -2,22 +2,24 @@ local component = require("component")
 local sides = require("sides")
 local shell = require("shell")
 local event = require("event")
-local reactorControl = require("reactorControl")
 
+local reactorControl = dofile(shell.resolve("reactorControl", "lua"))
 local timer = 0
 
 function start()
   if timer == 0 then
     timer = event.timer(0.2, reactorControl.mainLoop, math.huge)
+    print("id: ", timer)
   else
-    io.stderr:write("reactorControl already running")
+    print("reactorControl already running")
   end
 end
 
 function stop()
   local result = event.cancel(timer)
+  print("Stopped: ", result)
   if not result then
-    io.stderr:write("Error stopping reactorControl")
+    print("Error stopping reactorControl")
   end
   timer = 0
   local rs = component.redstone
--- reactor/reactorControl.lua
+++ reactor/reactorControl.lua
@@ -1,67 +1,65 @@
-reactorControl = {
-  local component = require("component")
-  local fs = require("filesystem")
-  local configlib = require("configlib")
-  local sides = require("sides")
+reactorControl = {}
+local component = require("component")
+local fs = require("filesystem")
+local configlib = require("configlib")
+local sides = require("sides")
 
-  if not component.isAvailable("redstone") then
-    io.stderr:write("No Redstone interface found")
-    return
-  elseif not component.isAvailable("reactor_redstone_port") then
-    io.stderr:write("No Reactor Found, connect an Adapter to a Reactor Redstone Port")
-    return
-  elseif not component.isAvailable("ag_steam_turbine") then
-    io.stderr:write("No Steam Turbine Found")
-    return
-  end
+if not component.isAvailable("redstone") then
+  io.stderr:write("No Redstone interface found")
+  return
+elseif not component.isAvailable("reactor_redstone_port") then
+  io.stderr:write("No Reactor Found, connect an Adapter to a Reactor Redstone Port")
+  return
+elseif not component.isAvailable("ag_steam_turbine") then
+  io.stderr:write("No Steam Turbine Found")
+  return
+end
 
-  local redstone = component.redstone
-  local reactor = component.reactor_redstone_port
-  local turbine = component.ag_steam_turbine
+local redstone = component.redstone
+local reactor = component.reactor_redstone_port
+local turbine = component.ag_steam_turbine
 
-  local defaultCfg = {
-    redstoneSide = "all",
-    stayUnderCapacity = .75,
-    reactorMaxHeat = .4,
-    reactorMinHeat = .3,
-  }
-
-  local config = configlib.loadConfig("reactor.cfg", defaultCfg)
-
-  local function setReactor(state)
-    if state then
-      local output = 15
-    else
-      local output = 0
-    end
-    if config.redstoneSide == "all" then
-      for _,s in ipairs(sides) do
-        redstone.setOutput(sides[s], output)
-      end
-    else
-      redstone.setOutput(sides[config.redstoneSide], output)
-    end
-  end
-
-  local function reactorHeat()
-    return reactor.getHeat() / reactor.getMaxHeat()
-  end
-
-  local function reactorOn()
-    return reactor.producesEnergy()
-  end
-
-  function mainLoop()
-    if turbine.getEnergyStored() < turbine.getEnergyCapacity() * config.stayUnderCapacity then
-      if reactorOn() then
-        if reactorHeat() >= config.reactorMaxHeat then setReactor(false) end
-      else
-        if reactorHeat() <= config.reactorMinHeat then setReactor(true) end
-      end
-    else
-      setReactor(false)
-    end
-  end
+local defaultCfg = {
+  redstoneSide = "all",
+  stayUnderCapacity = .75,
+  reactorMaxHeat = .4,
+  reactorMinHeat = .3,
 }
+
+local config = configlib.loadConfig("reactor.cfg", defaultCfg)
+
+local function setReactor(state)
+  local output = 0
+  if state then
+    output = 15
+  end
+  if config.redstoneSide == "all" then
+    for _,s in ipairs(sides) do
+      redstone.setOutput(sides[s], output)
+    end
+  else
+    redstone.setOutput(sides[config.redstoneSide], output)
+  end
+end
+
+local function reactorHeat()
+  return reactor.getHeat() / reactor.getMaxHeat()
+end
+
+local function reactorOn()
+  return reactor.producesEnergy()
+end
+
+function reactorControl.mainLoop()
+  if turbine.getEnergyStored() < turbine.getEnergyCapacity() * config.stayUnderCapacity then
+    if reactorOn() then
+      if reactorHeat() >= config.reactorMaxHeat then setReactor(false) end
+    else
+      if reactorHeat() <= config.reactorMinHeat then setReactor(true) end
+    end
+  else
+    setReactor(false)
+  end
+end
 
 return reactorControl
